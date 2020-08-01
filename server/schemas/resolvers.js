8b12206ba8221fce8f7b9a063dceb0cfd5d8f795
ktrnthsnr <asakthisner@hotmail.com>
2020-08-01 03:54:50 -0700

    added thought typeDef and resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,20 @@
+// import User and Thought models
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+          // helloWorld: () => {
+            // return 'Hello world!';
+
+        // thoughts: async () => {
+        //   return Thought.find().sort({ createdAt: -1 });
+
+        thoughts: async (parent, { username }) => {
+          const params = username ? { username } : {};
+          return Thought.find(params).sort({ createdAt: -1 });
       }
     }
   };
   
+  //export
   module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,45 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
+
+// Thought query
 const typeDefs = gql`
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
   type Query {
-    helloWorld: String
+    thoughts(username: String): [Thought]
   }
 `;
 
 // export the typeDefs
 module.exports = typeDefs;
+
+// // previous
+// type Query {
+//   thoughts: [Thought]
+// }
+
+
+// // previous
+// // create our typeDefs
+// const typeDefs = gql`
+//   type Query {
+//     // helloWorld: String?
+//   }
+// `;
+
