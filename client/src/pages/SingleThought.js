6bc9c661cb73a27223ec3317a7091f4488c12777
unknown <asakthisner@hotmail.com>
2020-08-08 03:06:54 -0700

    added react router to client app.js and nav link to replace a ref

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
+// new -- import the React hook, to access ID from the URL
+import { useParams } from 'react-router-dom';
+
 const SingleThought = props => {
+
+  // new -- to access ID from the URL from console.log
+    const { id: thoughtId } = useParams();
+    console.log(thoughtId);
+
   return (
     <div>
       <div className="card mb-3">
