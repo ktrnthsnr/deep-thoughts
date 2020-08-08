63fe93ec93f718bbdde1aa027c77c0e9e72bab59
unknown <asakthisner@hotmail.com>
2020-08-08 03:46:07 -0700

    added reactionlist component and exported to singlethought.js page

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,29 +1,87 @@
 import React from 'react';
 
+// new -- import from the new component ReactionList
+import ReactionList from '../components/ReactionList';
+
 // new -- import the React hook, to access ID from the URL
 import { useParams } from 'react-router-dom';
 
+// new -- useQuery hook to import from utils/queries
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHT } from '../utils/queries';
+
 const SingleThought = props => {
 
-  // new -- to access ID from the URL from console.log
-    const { id: thoughtId } = useParams();
-    console.log(thoughtId);
+  // new / prev -- initial  check to access ID from the URL from console.log
+    // const { id: thoughtId } = useParams();
+    // console.log(thoughtId);
 
-  return (
-    <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            Username
-          </span>{' '}
-          thought on createdAt
-        </p>
-        <div className="card-body">
-          <p>Thought Text</p>
+  // new -- after adding throught object ( id property on the variables object will become the $id param in the GraphQL query)
+    const { id: thoughtId } = useParams();
+    const { loading, data } = useQuery(QUERY_THOUGHT, {
+      variables: { id: thoughtId }
+    });
+    const thought = data?.thought || {};
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    // new -- after adding ReactionList component & thought object
+    return (
+      <div>
+       <div>
+          <div className="card mb-3">
+            <p className="card-header">
+              <span style={{ fontWeight: 700 }} className="text-light">
+                {thought.username}
+              </span>{' '}
+              thought on {thought.createdAt}
+            </p>
+            <div className="card-body">
+              <p>{thought.thoughtText}</p>
+            </div>
+          </div>
+          {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
         </div>
-      </div>
     </div>
-  );
-};
+    );
+  };
+
+
+  // // new -- after adding thought object, updated the JSX
+      //   return (
+      //     <div>
+      //       <div className="card mb-3">
+      //         <p className="card-header">
+      //           <span style={{ fontWeight: 700 }} className="text-light">
+      //             {thought.username}
+      //           </span>{' '}
+      //           thought on {thought.createdAt}
+      //         </p>
+      //         <div className="card-body">
+      //           <p>{thought.thoughtText}</p>
+      //         </div>
+      //       </div>
+      //   </div>
+      //   );
+      // };
+
+// // before
+    //   return (
+    //     <div>
+    //       <div className="card mb-3">
+    //         <p className="card-header">
+    //           <span style={{ fontWeight: 700 }} className="text-light">
+    //             Username
+    //           </span>{' '}
+    //           thought on createdAt
+    //         </p>
+    //         <div className="card-body">
+    //           <p>Thought Text</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   );
+    // };
 
 export default SingleThought;
